1a0ce76806a78c120c72f41c6a13b93eab09446b
Mücahit Kökdemir <76880536+mucahitkok@users.noreply.github.com>
2022-10-13 08:07:39 +0300

    Firebase adapter eklendi

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { FirestoreAdapter } from "@next-auth/firebase-adapter";
 
 export const authOptions = {
   providers: [
@@ -12,15 +13,44 @@ export const authOptions = {
   pages: {
     signIn: "/auth/signin",
   },
+  adapter: FirestoreAdapter({
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID,
+  }),
 
   callbacks: {
-    async session({ session, token }) {
+    async session({ session, user }) {
       session.user.username = session.user.name
         .split(" ")
         .join("")
-        .toLocaleLowerCase();
-      session.user.uid = token.sub;
-      session.user.tokenData = token;
+        .toLowerCase()
+        .replace(/[^a-zA-Z0-9]/g, function (s) {
+          var c = s.charCodeAt(0);
+          switch (c) {
+            case 231:
+              return "c";
+            case 287:
+              return "g";
+            case 305:
+              return "i";
+            case 351:
+              return "s";
+            case 252:
+              return "u";
+            case 231:
+              return "c";
+            case 246:
+              return "o";
+            default:
+              return "";
+          }
+        });
+      session.user.uid = user.id;
       return session;
     },
   },
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,14 @@
 import Head from "next/head";
 import Link from "next/link";
-import { getProviders, signIn, useSession, signOut } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
   if (session) {
     return (
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1 className="text-6xl font-bold">
-          Hoşgeldin {session.user.name}
-        </h1>
+        <h1 className="text-6xl font-bold">Hoşgeldin {session.user.name}</h1>
+        <img src={session.user.image} alt="profile" />
         <button
           onClick={() => signOut()}
           className="p-6 mt-6 text-2xl font-bold text-white bg-blue-500 hover:opacity-95 rounded-lg"
